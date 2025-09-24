d81e6b8cfca3f05675bfe084bd228a9991272013
Sahil Achhava <sahil.achhava@koneksys.com>
2025-09-24 12:56:46 +0530

    removed tests

--- test/cmake/mqtt_interoperability/run.sh
+++
@@ -1,6 +0,0 @@
-eclipse-threadx#!/bin/bash
-
-cd $(dirname $0)
-
-[ -f .run.sh ] || ln -sf ../threadx/scripts/cmake_bootstrap.sh .run.sh
-CTEST_PARALLEL_LEVEL=1 ENABLE_IDLE=ON ./.run.sh $*
--- test/cmake/ptp/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd $(dirname $0)
-
-[ -f .run.sh ] || ln -sf ../threadx/scripts/cmake_bootstrap.sh .run.sh
-PTP_ONLY=ON ./.run.sh $*
